809173949bb1aa708cd66c202874379bd7035d91
ArthurKouam <gkouamarthur@gmail.com>
2025-03-26 19:07:41 +0100

    Fix icon

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  assetsInclude: ['**/*.svg'],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
